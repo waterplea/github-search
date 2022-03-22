7b59e1b8fa40e6771e6e326a53e497af940bfe74
waterplea <alexander@inkin.ru>
2022-03-22 18:53:34 +0300

    chore: add Taiga UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TuiNotificationsModule, TuiRootModule } from '@taiga-ui/core';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' })
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiNotificationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
